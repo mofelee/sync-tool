8cde5cce442e18dda7eefb34a687022762593b5e
MofeLee <mofe@me.com>
2018-06-01 11:52:50 +0800

    add log

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # sync-tool
+
+```
+yarn
+cp config.example.js config.js # edit the config
+```
+
+```
+node index.js
+```
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const shell = require('shelljs');
 const _ = require('lodash');
+const signale = require('signale');
 
 const repoMap = require('./config');
 
 shell.exec('mkdir -p repos')
 
 _.each(repoMap, d=>{
+  signale.start(d.name);
+
   const path = `repos/${d.name}`;
 
   // 目录不存在创建目录
@@ -18,4 +21,6 @@ _.each(repoMap, d=>{
   shell.exec(`cd ${path} && git remote add fork ${d.fork} `)
 
   shell.exec(`cd ${path} && git push -u fork master`)
+
+  signale.success(d.name);
 });
